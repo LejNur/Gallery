0dada9f45a21fac625bb92429678d36960f3e660
Lejla <lejla.nrk@gmail.com>
2024-03-19 14:27:56 +0100

    feat:components/card/input

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
+  @Input() classImg = '';
+  @Input() hoverState = '';
+  @Input() text = '';
+  @Input() authorName = '';
+  @Input() buttonClass = '';
 
+  @Output() clickGallery = new EventEmitter<string>();
+
+  fullScreen(img: string) {
+    this.clickGallery.emit(img);
+  }
 }
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrl: './input.component.css',
 })
 export class InputComponent {
+  @Input() myPlaceholder = '';
+  @Input() inputClass = '';
+  @Input() buttonClass = '';
+  @Input() containerClass = '';
 
+  @Output() newClickEvent = new EventEmitter<string>();
+
+  onInput(value: string) {
+    this.newClickEvent.emit(value);
+  }
 }
