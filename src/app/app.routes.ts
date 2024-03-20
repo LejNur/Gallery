f7e1cf9e40e896a06edd699d06dd696071edfb32
Lejla <lejla.nrk@gmail.com>
2024-03-20 13:05:11 +0100

    styles:routerLinkActive modify

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { GalleryComponent } from './pages/gallery/gallery.component';
 import { MapComponent } from './pages/map/map.component';
 
 export const routes: Routes = [
-  { path: '', component: GalleryComponent, pathMatch: 'full' },
+  { path: '', component: GalleryComponent },
   { path: 'map', component: MapComponent },
 ];
